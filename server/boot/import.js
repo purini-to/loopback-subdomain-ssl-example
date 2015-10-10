060bbd15799ae6c5e4c990b91c1f4e21b9269be2
urainkyo <hs_urainkyo@opt.co.jp>
2015-10-10 23:24:41 +0900

    bootスクリプトを変更して、lib配下を参照するように修正

---
+++ server/boot/import.js
@@ -0,0 +1,22 @@
+var requireDir = require('require-dir');
+
+/**
+ * 再帰的に関数が見つかるまで探索を行う
+ * 関数が見つかったら関数の実行を行う
+ * @param  {object} importObj 探索対象
+ * @param  {object} app       loopbackアプリケーション
+ */
+var deepImport = function (importObj, app) {
+  Object.keys(importObj).forEach(function (key) {
+    if (typeof importObj[key] !== 'function') {
+      deepImport(importObj[key], app);
+    } else {
+      importObj[key](app);
+    }
+  });
+};
+
+module.exports = function (app) {
+  var imports = requireDir('../lib', {recurse: true});
+  deepImport(imports, app);
+};
