f3e52060cd0dd948b60420cf31653dd9468cfa0d
urainkyo <hs_urainkyo@opt.co.jp>
2015-10-25 18:44:45 +0900

    メッセージモデルとチャンネルのACLを作成

--- server/boot/role/teamMemberResolver.js
+++ server/boot/role/teamMemberResolver.js
@@ -1,3 +1,63 @@
+/**
+ * ユーザーIDを取得する
+ * @param  {[type]}   context コンテキスト
+ * @return {Integer}          ユーザーID
+ */
+function findUserByToken(context) {
+  var userId = context.accessToken.userId;
+  return userId;
+}
+
+/**
+ * 捜査対象のチームにユーザーが所属しているか検証する
+ * @param  {[type]}   context コンテキスト
+ * @param  {Function} cb      判定コールバック
+ * @param  {Function} ecb     エラー時のコールバック
+ */
+function verificationByTeam(context, cb, ecb) {
+  var userId = findUserByToken(context);
+  if (!userId) return ecb();
+
+  context.model.findById(context.modelId, function(err, team) {
+    if (err || !team) return ecb();
+
+    team.users.findById(userId, function(err, user) {
+      if (err) {
+        console.error(err);
+        return cb(null, false);
+      }
+      cb(null, user); // true = is a team member
+    });
+  });
+}
+
+/**
+ * チャンネルのチームがユーザーの所属チームか検証する
+ * @param  {[type]}   context コンテキスト
+ * @param  {Function} cb      判定コールバック
+ * @param  {Function} ecb     エラー時のコールバック
+ */
+function verificationByChannel(context, cb, ecb) {
+  var userId = findUserByToken(context);
+  if (!userId) return ecb();
+
+  context.model.findById(context.modelId, function(err, channel) {
+    if (err || !channel) return ecb();
+
+    channel.team(channel.teamId, function(err, team) {
+      if (err || !team) return ecb();
+
+      team.users.findById(userId, function(err, user) {
+        if (err) {
+          console.error(err);
+          return cb(null, false);
+        }
+        cb(null, user); // true = is a team member
+      });
+    });
+  });
+}
+
 module.exports = function(app) {
   var Role = app.models.Role;
 
@@ -9,26 +69,13 @@ module.exports = function(app) {
     }
 
     // if the target model is not project
-    if (context.modelName !== 'team') {
+    if (context.modelName === 'team') {
+      verificationByTeam(context, cb, reject);
+    } else if (context.modelName === 'channel') {
+      verificationByChannel(context, cb, reject);
+    } else {
       return reject();
     }
 
-    // do not allow anonymous users
-    var userId = context.accessToken.userId;
-    if (!userId) {
-      return reject();
-    }
-
-    context.model.findById(context.modelId, function(err, team) {
-      if (err || !team) return reject();
-
-      team.users.findById(userId, function(err, user) {
-        if (err) {
-          console.error(err);
-          return cb(null, false);
-        }
-        cb(null, user); // true = is a team member
-      });
-    });
   });
 };
